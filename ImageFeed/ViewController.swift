89c8b7b89280f16c2aaf61c509c32f89f010c5c7
Irina Golubinskaya <irinafesenko0709@yandex.ru>
2023-08-24 17:09:27 +0300

    !

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Irina Golubinskaya on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
